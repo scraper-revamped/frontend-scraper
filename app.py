fd140ec15166d8c7fa83518ff92a7e3b27d79164
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-08-18 15:16:43 +0300

    changed the bucket name cuz of new acc

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ else:
     print("USER_DATA not found in the environment variables.")
 
 # Google Cloud Storage configuration
-BUCKET_NAME = "scraping_revamped_2"
+BUCKET_NAME = "scraping_revamped_4"
 
 # Helper function to check credentials
 def check_credentials(username, password):
